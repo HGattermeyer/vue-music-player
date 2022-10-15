ad8e21a81149bcd8836d06dd6666b980902c1118
Henrique <henrique1989@gmail.com>
2022-10-15 10:43:50 +0100

    first commit

---
+++ src/includes/firebase.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA2noYNuahnd9yWXfC4AmQ9s6WC3npgonA",
+  authDomain: "music-372bc.firebaseapp.com",
+  projectId: "music-372bc",
+  storageBucket: "music-372bc.appspot.com",
+  appId: "1:271816236313:web:9fe1cc6b47f705b3613988",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+const usersCollection = db.collection("users");
+const songsCollection = db.collection("songs");
+
+export { auth, db, usersCollection, songsCollection, storage };
